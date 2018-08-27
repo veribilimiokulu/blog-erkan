b31ff72df5e92d3dc6b295c36b1aba1d46003473
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-08-27 07:14:07 +0300

    1. commit: It works without TrainValidationSplit rmse score is also 0.91

---
+++ erkan/spark-als/src/main/scala/spark/ml/recommendation/als/ErkansALS.scala
@@ -0,0 +1,87 @@
+package spark.ml.recommendation.als
+
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.ml.tuning.{TrainValidationSplit, ParamGridBuilder}
+import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object ErkansALS {
+  def main(args: Array[String]): Unit = {
+/*
+   val sparkConf = new SparkConf()
+
+     .setExecutorEnv("spark.driver.memory","4g")
+     .setExecutorEnv("spark.executor.memory","8g")
+     .setExecutorEnv("spark.sql.broadcastTimeout","1200")
+     .setExecutorEnv("spark.eventLog.enabled","false")
+*/
+  val spark = SparkSession.builder()
+    .master("local[*]")
+    .appName("SparkALS")
+    .getOrCreate()
+
+    val movieRatings = spark.read.format("csv")
+      .option("header","true")
+      .option("inferSchema","true")
+      .load("C:\\Users\\toshiba\\SkyDrive\\veribilimi.co\\Datasets\\ml-latest-small\\ratings.csv")
+      .drop("timestamp")
+     // .sample(0.1,142)
+
+   movieRatings.show()
+
+
+ println(movieRatings.count())
+
+// 100.004 adet rating var.
+
+    // Create training and test set
+    val Array(training, test) = movieRatings.randomSplit(Array(0.8, 0.2),seed = 142)
+training.cache()
+
+    // Create ALS model
+    val alsObject = new ALS()
+      .setUserCol("userId")
+      .setItemCol("movieId")
+      .setRatingCol("rating")
+      .setColdStartStrategy("drop")
+      .setNonnegative(true)
+
+/*
+    // Tune model using ParamGridBuilder
+    val paramGridObject = new ParamGridBuilder()
+      .addGrid(alsObject.rank, Array(14))
+      .addGrid(alsObject.maxIter, Array(20))
+      .addGrid(alsObject.regParam, Array(.19))
+      .build()
+*/
+    // Define evaluator as RMSE
+    val evaluator = new RegressionEvaluator()
+      .setMetricName("rmse")
+      .setLabelCol("rating")
+      .setPredictionCol("prediction")
+/*
+    // Build cross validation using TrainValidationSplit
+    val tvs = new TrainValidationSplit()
+      .setEstimator(alsObject)
+      .setEstimatorParamMaps(paramGridObject)
+      .setEvaluator(evaluator)
+*/
+    // Fit ALS model to training set
+    val model = alsObject.fit(training)
+/*
+        // Take best model
+        val bestModel = model.bestModel
+*/
+        // Generate predictions and evaluate RMSE
+        val predictions = model.transform(test)
+        val rmse = evaluator.evaluate(predictions)
+
+        predictions.show()
+
+        // Print evaluation metrics and model parameters
+        println("RMSE = ", rmse)
+
+
+  }
+}
